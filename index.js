e39eaf520367cb221a5cb6d613d6f95fca75456a
Tayebi Mohammed Ayoub <tayebiayoub24@gmail.com>
2024-06-22 19:06:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jun 23, 204',
+  birthdate: 'Jun 23, 2024',
   name: 'warda'
 };
 
